37113293932297397661ba74814b98ebdabef0a3
HaowenYang2004 <haowenyang@ufl.edu>
2025-07-29 23:48:01 -0400

    Merge Changes

--- test_calculator.py
+++ test_calculator.py
@@ -12,12 +12,15 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    def test_multiply(self):
+        self.assertEqual(mul(2,3),6)
+        self.assertEqual(mul(-1, 5),-5)
+        self.assertEqual(mul(0, 100),0)
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_divide(self):
+        self.assertEqual(div(2,10),5)
+        self.assertAlmostEqual(div(3,7),2.333333, places=5)
+        self.assertEqual(div(2,-8),-4)
 
     ######## Partner 2
     # def test_divide_by_zero(self): # 1 assertion
@@ -33,25 +36,30 @@ class TestCalculator(unittest.TestCase):
     #     # use same technique from test_divide_by_zero
     #     fill in code
     # ##########################
-    
+
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+    def test_log_invalid_argument(self):
+        # log(a, b): base=a, value=b
+        # log(1, 5): base cannot be 1, should raise
+        with self.assertRaises(ValueError):
+            log(1, 5)
+        # log(-2, 5): base cannot be negative, should raise
+        with self.assertRaises(ValueError):
+            log(-2, 5)
+        # log(2, -5): value cannot be negative, should raise
+        with self.assertRaises(ValueError):
+            log(2, -5)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
+    def test_hypotenuse(self):
+        self.assertAlmostEqual(hypotenuse(3, 4), 5.0, places=5)
+        self.assertAlmostEqual(hypotenuse(5, 12), 13.0, places=5)
+        self.assertAlmostEqual(hypotenuse(8, 15), 17.0, places=5)
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
-    ##########################
-
+    def test_sqrt(self):
+        with self.assertRaises(ValueError):
+            square_root(-4)
+        self.assertEqual(square_root(9), 3)
+        self.assertAlmostEqual(square_root(2), 1.414213, places=5)
 # Do not touch this
 if __name__ == "__main__":
     unittest.main()
